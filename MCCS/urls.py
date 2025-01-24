bb31e07abc9be23c5eb55a6eb7c04e8bde6580f2
sa001gar <kundusagar233@gmail.com>
2025-01-25 03:41:26 +0530

    Minor Bug fixes

--- MCCS/urls.py
+++ MCCS/urls.py
@@ -18,7 +18,15 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls'))
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
