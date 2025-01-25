713b91e452cc539b422f552379ac34a9fa90f294
sa001gar <kundusagar233@gmail.com>
2025-01-26 02:07:48 +0530

    update the endpoints

--- MCCS/settings.py
+++ MCCS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','13.233.152.122']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','13.233.152.122','https://redesign-mccs.sagarkundu.live/','redesign-mccs.sagarkundu.live']
 
 LOGGING = {
     'version': 1,
