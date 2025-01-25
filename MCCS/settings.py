07ee41e2db9494f70147437533576564c4a1a51b
sa001gar <kundusagar233@gmail.com>
2025-01-26 01:18:08 +0530

    changes the staticfiles dir

--- MCCS/settings.py
+++ MCCS/settings.py
@@ -140,9 +140,11 @@ import os
 
 # Static files settings
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # This is for production
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
